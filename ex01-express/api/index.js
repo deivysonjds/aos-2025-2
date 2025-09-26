13807a33ff85a9524b2fb93b8a8441fd99afffea
Deivyson Silva <deivyson.jd.silva@gmail.com>
2025-09-26 01:33:44 -0300

    added: routes messages with DB

--- ex01-express/api/index.js
+++ ex01-express/api/index.js
@@ -24,14 +24,6 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Código para injetar no context o usuário que está logado e os models
-app.use(async (req, res, next) => {
-  req.context = {
-    models,
-    me: await models.User.findByPk(1),
-  };
-  next();
-});
 
 app.use("/", routes.root);
 app.use("/session", routes.session);
@@ -48,7 +40,7 @@ sequelize.sync({ force: eraseDatabaseOnSync }).then(async () => {
   }
 
   app.listen(port, () => {
-    console.log(`Example app listening on port ${port}!`);
+    console.log(`Server is running in http://localhost:${port} !`);
   });
 });
 
