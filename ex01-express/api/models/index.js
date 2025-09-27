a1a8d5619bf76fee97012a5808ade957c94de0f6
Deivyson Silva <deivyson.jd.silva@gmail.com>
2025-09-27 20:29:39 -0300

    refactor: pg insert in sequelize configuration

--- ex01-express/api/models/index.js
+++ ex01-express/api/models/index.js
@@ -1,5 +1,5 @@
 import Sequelize from "sequelize";
-
+import pg from "pg"
 import getUserModel from "./user";
 import getMessageModel from "./message";
 
@@ -15,7 +15,7 @@ const sequelize = new Sequelize(process.env.POSTGRES_URL, {
       rejectUnauthorized: false,
     },
   },
-  dialectModule: require("pg"),
+  dialectModule: pg,
   logging: console.log
 });
 
