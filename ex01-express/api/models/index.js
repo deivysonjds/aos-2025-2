fb2633f29caa83942f6efe8d0eda3f0ee5b72904
Deivyson Silva <deivyson.jd.silva@gmail.com>
2025-09-27 20:43:16 -0300

    refactor: corrected imports

--- ex01-express/api/models/index.js
+++ ex01-express/api/models/index.js
@@ -1,7 +1,7 @@
 import Sequelize from "sequelize";
 import pg from "pg"
-import getUserModel from "./user";
-import getMessageModel from "./message";
+import getUserModel from "./user.js";
+import getMessageModel from "./message.js";
 
 //POSTGRES_URL
 const sequelize = new Sequelize(process.env.POSTGRES_URL, {
