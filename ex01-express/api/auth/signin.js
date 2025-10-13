f7022f6290fa15ca5d29d0633828773bc2fb33f6
Deivyson J. Silva <139878479+deivysonjds@users.noreply.github.com>
2025-10-13 22:16:16 +0000

    refactor: secret in verify from argon

--- ex01-express/api/auth/signin.js
+++ ex01-express/api/auth/signin.js
@@ -1,26 +1,34 @@
-import models from "../models/index.js"
-import createToken from "../services/createToken.js"
-import {Router} from "express"
-import argon2d from "argon2"
+import models from "../models/index.js";
+import createToken from "../services/createToken.js";
+import { Router } from "express";
+import argon2d from "argon2";
 
-const router = Router()
+const router = Router();
 
-router.post("/", async (req, res)=>{
-    const {email, senha} = req.body
+router.post("/", async (req, res) => {
+    const { email, senha } = req.body;
 
-    let user = await models.User.findByLogin(email)
+    try {
+        let user = await models.User.findByLogin(email);
 
-    if(!user){
-        return res.status(404).send({error: "credenciais inválidas"})
+        if (!user) {
+            return res.status(404).send({ error: "credenciais inválidas" });
+        }
+
+        let isValid = await argon2d.verify(user.senha, senha, {
+            secret: process.env.PEPPER_SECRET
+        });
+
+        if (!isValid) {
+            return res.status(401).send({ error: "credenciais inválidas" });
+        }
+
+        let tokens = await createToken({ id: user.id });
+
+        return res.status(200).send(tokens);
+    } catch (error) {
+        return res.status(500).send({ error: "Erro interno no servidor" });
     }
+});
 
-    let isValid = argon2d.verify(user.senha, senha)
-
-    if(!isValid) return res.status(401).send({error: "credenciais inválidas"})
-
-    let tokens = createToken({id: user.id})
-
-    return res.status(200).send(tokens)
-})
-
-export default router
+export default router;
